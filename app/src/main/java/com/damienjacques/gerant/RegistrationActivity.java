2f4fe5d6d1feaefecc7d06ad35cfbb3ac9ff814f
DamienJacques900 <damien.jacques900@gmail.com>
2016-11-06 15:54:19 +0100

    inscription

--- app/src/main/java/com/damienjacques/gerant/RegistrationActivity.java
+++ app/src/main/java/com/damienjacques/gerant/RegistrationActivity.java
@@ -17,7 +17,7 @@ public class RegistrationActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_registrationclient);
     }
 
     @Override
