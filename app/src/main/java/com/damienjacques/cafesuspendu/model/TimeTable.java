d96e4d952f4772614bda425c21c296737fdb6b7e
DamienJacques900 <damien.jacques900@gmail.com>
2016-11-22 10:15:01 +0100

    @nonNull timetable usercafe userperson

--- app/src/main/java/com/damienjacques/cafesuspendu/model/TimeTable.java
+++ app/src/main/java/com/damienjacques/cafesuspendu/model/TimeTable.java
@@ -1,15 +1,22 @@
 package com.damienjacques.cafesuspendu.model;
 
 import android.icu.util.DateInterval;
+import android.support.annotation.*;
 
 import java.io.Serializable;
 
 public class TimeTable implements Serializable
 {
+
     private Integer timeTableID;
+
     private DateInterval openingHour;
+
     private DateInterval closingHour;
+
+    @NonNull
     private Integer dayOfWeek;
 
+    @NonNull
     private UserCafe userCafe;
 }
--- app/src/main/java/com/damienjacques/cafesuspendu/model/UserCafe.java
+++ app/src/main/java/com/damienjacques/cafesuspendu/model/UserCafe.java
@@ -1,20 +1,45 @@
 package com.damienjacques.cafesuspendu.model;
 
+import android.support.annotation.*;
+
 import java.io.Serializable;
 import java.util.List;
 
 
 public class UserCafe implements Serializable
 {
+    @NonNull
+    @Size(min=6, max=20)
     private Integer userCafeId;
+
+    @NonNull
+    @Size(min=6, max=20)
     private String password;
+
+    @NonNull
+    @Size(max=30)
     private String cafeName;
+
+    @NonNull
+    @Size(max=50)
     private String street;
+
+    @NonNull
+    @Size(max=5)
     private String number;
+
+    @NonNull
     private Integer nbCoffeeRequiredForPromotion;
+
+    @NonNull
     private Double promotionValue;
+
+
     private List<Booking> bookings;
+
+
     private List<Charity> charities;
+
 
     private List<TimeTable> timeTables;
 }
